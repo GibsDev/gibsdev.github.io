e97439ea18c6a60afd9e334bf58f55ebeb8088d0
GibsDev <notcurtisg@gmail.com>
2026-03-23 02:30:00 -0400

    switch to docs

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig(({ command }) => ({
   plugins: [react()],
   base: command === 'serve' ? '/' : basePath(),
   build: {
+    outDir: 'docs',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
